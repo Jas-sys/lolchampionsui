55e00e7a1b6a9a53b08767b5bd87c5c101503e6e
Jas-sys <61457194+Jas-sys@users.noreply.github.com>
2022-10-13 12:04:48 +0100

    Bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://spring-boot-ose1-project.apps.ose.openshift.conygre.com:443/api/v1/lolchampion/"
+  apiUrl: "https://spring-boot-ose1-project.apps.ose.openshift.conygre.com:443/api/v1/"
 };
